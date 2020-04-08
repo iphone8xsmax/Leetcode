ab57e1fd5489ee0fd78102296804668935f54765
iphone8xsmax <18183124885@163.com>
2020-04-08 19:39:38 +0800

    104 二叉树的最大深度

---
+++ tree/104 二叉树的最大深度.go
@@ -0,0 +1,25 @@
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil{
+        return 0
+    }
+    lDepth := maxDepth(root.Left)
+    rDepth := maxDepth(root.Right)
+    if lDepth > rDepth{
+        return lDepth + 1
+    }
+    return rDepth + 1
+}
